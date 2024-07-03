c57e32e9d3202d9aac9965ed6bcdc03c9b6f222d
Maidana <franco_maidana07@hotmail.com>
2024-07-03 13:10:57 -0300

    - Configuración de la estructura y de las dependencias basicas del proyecto.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
+
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
