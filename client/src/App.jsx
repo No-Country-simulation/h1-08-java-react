d7520e289ab5472449955c04946ba42d9eb5f2cf
Maidana <franco_maidana07@hotmail.com>
2024-07-08 16:06:12 -0300

    - Configuración de react-query

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
-import NotFound from './pages/NotFound';
-import Home from './pages/Home';
+import { QueryClient, QueryClientProvider, } from '@tanstack/react-query'
 import { Route, Switch } from "wouter";
 import Navbar from './layout/Navbar';
 import Footer from './layout/Footer';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
 
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Navbar />
       <main>
         <Switch>
@@ -23,7 +25,7 @@ function App() {
         </Switch>
       </main>
       <Footer />
-    </>
+    </QueryClientProvider>
   );
 }
 
