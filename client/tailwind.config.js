b92fa4116acfbd08c197cbca29476e66de30919a
gabyp05 <gabyapd05@gmail.com>
2024-07-09 13:51:33 -0400

    Agregado colores base

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from "daisyui"
+import daisyui from "daisyui";
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightGreen: "#B9EAE8",
+        darkGreen: "#17B8B2",
+        textColor: "#232626",
+        white: "#F0F9F8",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
   },
-  plugins: [
-    daisyui,
-  ],
-}
+  plugins: [daisyui],
+};
