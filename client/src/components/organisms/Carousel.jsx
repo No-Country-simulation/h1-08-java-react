1eb8a21166d5c42b5e7d86572bfeb50653247218
gabyp05 <gabyapd05@gmail.com>
2024-07-11 15:28:42 -0400

    hover indicadores

--- client/src/components/organisms/Carousel.jsx
+++ client/src/components/organisms/Carousel.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import { image1, image2, image3 } from "../../assets";
 
 const Carousel = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleIndicatorClick = (index) => {
+    setActiveIndex(index);
+  };
+
   return (
     <div className="flex flex-col gap-4">
       <div className="carousel carousel-center rounded-box max-w-[350px] max-h-[180px] space-x-2">
@@ -15,9 +22,9 @@ const Carousel = () => {
         </div>
       </div>
       <div className="flex w-full justify-center gap-2 py-2">
-        <a href="#item1" className="rounded-full size-2 bg-darkOrange hover:bg-black"></a>
-        <a href="#item2" className="rounded-full size-2 bg-darkOrange hover:bg-black"></a>
-        <a href="#item3" className="rounded-full size-2 bg-darkOrange hover:bg-black"></a>
+        <a href="#item1" className={`rounded-full size-2 ${activeIndex === 0 ? 'bg-black' : 'bg-darkOrange'} hover:bg-black`} onClick={() => handleIndicatorClick(0)}></a>
+        <a href="#item2" className={`rounded-full size-2 ${activeIndex === 1 ? 'bg-black' : 'bg-darkOrange'} hover:bg-black`} onClick={() => handleIndicatorClick(1)}></a>
+        <a href="#item3" className={`rounded-full size-2 ${activeIndex === 2 ? 'bg-black' : 'bg-darkOrange'} hover:bg-black`} onClick={() => handleIndicatorClick(2)}></a>
       </div>
     </div>
   );
