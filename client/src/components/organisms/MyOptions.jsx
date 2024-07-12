9404b1ccac73e608b861ddcc85de94c67bfcdd1d
gabyp05 <gabyapd05@gmail.com>
2024-07-11 20:01:10 -0400

    Añadiendo links

--- client/src/components/organisms/MyOptions.jsx
+++ client/src/components/organisms/MyOptions.jsx
@@ -1,18 +1,29 @@
-import CardButton from "../molecules/CardButton"
-import { stethoscope, electrocardiogram, doctor, capsule} from "../../assets";
+import CardButton from "../molecules/CardButton";
+import { stethoscope, electrocardiogram, doctor, capsule } from "../../assets";
+import { Link } from "wouter";
 
 const MyOptions = () => {
   return (
     <div>
-        <h1 className="text-textColor text-2xl font-poppins font-semibold">Mis carpeta de salud</h1>
-        <div className="grid grid-cols-2 gap-4 py-5">
-            <CardButton title="Vacunas" image={electrocardiogram} link="#"/>
-            <CardButton title="Medicamentos" image={capsule} link="#"/>
-            <CardButton title="Historial Clínico" image={stethoscope} link="#"/>
-            <CardButton title="Mis Citas" image={doctor} link="#"/>
-        </div>
+      <h1 className="text-textColor text-2xl font-poppins font-semibold">
+        Mis carpeta de salud
+      </h1>
+      <div className="grid grid-cols-2 gap-4 py-5">
+        <Link to="#">
+          <CardButton title="Vacunas" image={electrocardiogram} link="#" />
+        </Link>
+        <Link to="#">
+          <CardButton title="Medicamentos" image={capsule} link="#" />
+        </Link>
+        <Link to="historial">
+          <CardButton title="Historial Clínico" image={stethoscope} link="#" />
+        </Link>
+        <Link to="#">
+          <CardButton title="Mis Citas" image={doctor} link="#" />
+        </Link>
+      </div>
     </div>
-  ) 
-}
+  );
+};
 
-export default MyOptions
+export default MyOptions;
