04455e2f97f318d65473a732811be805c26de121
gabyp05 <gabyapd05@gmail.com>
2024-07-16 14:15:09 -0400

    agregado condicional al p description

--- client/src/components/molecules/SectionCard.jsx
+++ client/src/components/molecules/SectionCard.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 const SectionCard = ({ sectionName, sectionIcon, altIcon, description }) => {
     return (
         <div className="text-black px-2 py-6 rounded-2xl bg-light backdrop-blur-sm card-shadow border border-orange transition duration-200 ease hover:border-fucsia hover:bg-white w-9/12 min-w-[350px] mb-5">
-            <h1 className="font-bold text-3xl font-poppins px-2 max-w-[290px] leading-10">
+            <h1 className="font-bold text-2xl md:text-3xl font-poppins px-2 max-w-[300px] leading-10">
                 {sectionName}
             </h1>
 
@@ -14,16 +14,18 @@ const SectionCard = ({ sectionName, sectionIcon, altIcon, description }) => {
                 </div>
             }
 
-            <p className="font-roboto text-xl my-1 p-3 leading-7">
-                {description}
-            </p>
+            {description &&
+                <p className="font-roboto text-xl my-1 p-3 leading-7">
+                    {description}
+                </p>
+            }
         </div>
     )
 }
 
 SectionCard.propTypes = {
     sectionName: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
+    description: PropTypes.string,
     sectionIcon: PropTypes.string || undefined || PropTypes.any,
     altIcon: PropTypes.string || undefined,
 };
