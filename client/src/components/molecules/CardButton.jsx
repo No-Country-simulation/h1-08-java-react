af2df1169d17b058303d3a7c5ced708ee801d0c3
gabyp05 <gabyapd05@gmail.com>
2024-07-15 20:44:45 -0400

    refactor

--- client/src/components/molecules/CardButton.jsx
+++ client/src/components/molecules/CardButton.jsx
@@ -1,18 +1,21 @@
 import PropTypes from "prop-types";
+import { useLocation } from "wouter";
 
-const CardButton = ({ image, title }) => {
+const CardButton = ({ image, title, link }) => {
+  // eslint-disable-next-line no-unused-vars
+  const [location, setLocation] = useLocation();
   return (
-      <div className="card rounded-2xl bg-light backdrop-blur-sm border border-orange size-[167px] flex items-center justify-center p-1.5 card-shadow">
+      <div className="card rounded-2xl bg-light backdrop-blur-sm border border-orange size-[167px] flex items-center justify-center p-1.5 card-shadow cursor-pointer" onClick={() => setLocation(link)}>
         <div className="flex flex-col gap-5 items-center">
           <div className="bg-lightGreen border border-black rounded-full size-20 flex items-center justify-center">
             <img src={image} alt={title} className="size-16" />
           </div>
           <div>
-            <span
+            <a href={link}
               className="text-center font-poppins font-semibold text-lg"
             >
               {title}
-            </span>
+            </a>
           </div>
         </div>
       </div>
--- client/src/components/organisms/MyOptions.jsx
+++ client/src/components/organisms/MyOptions.jsx
@@ -1,6 +1,5 @@
 import CardButton from "../molecules/CardButton";
 import { stethoscope, electrocardiogram, doctor, capsule } from "../../assets";
-import { Link } from "wouter";
 
 const MyOptions = () => {
   return (
@@ -9,18 +8,10 @@ const MyOptions = () => {
         Mi carpeta de salud
       </h1>
       <div className="grid grid-cols-2 gap-4 py-5">
-        <Link to="#">
-          <CardButton title="Vacunas" image={electrocardiogram} link="#" />
-        </Link>
-        <Link to="#">
-          <CardButton title="Medicamentos" image={capsule} link="#" />
-        </Link>
-        <Link to="historial">
-          <CardButton title="Historial Clínico" image={stethoscope} link="#" />
-        </Link>
-        <Link to="#">
-          <CardButton title="Mis Citas" image={doctor} link="#" />
-        </Link>
+        <CardButton title="Vacunas" image={electrocardiogram} link="vacunas" />
+        <CardButton title="Medicamentos" image={capsule} link="#" />
+        <CardButton title="Historial Clínico" image={stethoscope} link="historial" />
+        <CardButton title="Mis Citas" image={doctor} link="#" />
       </div>
     </div>
   );
