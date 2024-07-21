7c40e8394cc13d5b16d8ddd26f873a5786153e8c
Miguel Angel Chavez <chavez.miguel.angel154@gmail.com>
2024-07-21 20:54:50 -0300

    Documentación de Swagger

--- backend/src/main/java/io/hackathon/justina/auth/controller/AuthController.java
+++ backend/src/main/java/io/hackathon/justina/auth/controller/AuthController.java
@@ -7,12 +7,14 @@ import io.hackathon.justina.auth.services.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Validated
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
--- backend/src/main/java/io/hackathon/justina/doctor/helper/DoctorMapper.java
+++ backend/src/main/java/io/hackathon/justina/doctor/helper/DoctorMapper.java
@@ -1,11 +1,15 @@
 package io.hackathon.justina.doctor.helper;
 
 import io.hackathon.justina.auth.models.dto.request.RegisterRequest;
+import io.hackathon.justina.doctor.models.Especialidad;
 import io.hackathon.justina.doctor.models.Medico;
 import io.hackathon.justina.doctor.models.dto.DoctorDto;
 import io.hackathon.justina.utils.Role;
+import io.hackathon.justina.utils.modelMapper.Mapper;
 
 public class DoctorMapper {
+
+    private static final Mapper mapper = Mapper.getInstance();
 
     private DoctorMapper() {}
     public static Medico toMedico(RegisterRequest medico) {
@@ -17,7 +21,7 @@ public class DoctorMapper {
                 .telefono(medico.getTelefono())
                 .password(medico.getPassword())
                 .role(Role.DOCTOR)
-                .especialidad(medico.getDoctor().getEspecialidad())
+                .especialidad(mapper.map(medico.getDoctor().getEspecialidad(), Especialidad.class))
                 .numeroLicencia(medico.getDoctor().getNumeroLicencia())
                 .build();
     }
