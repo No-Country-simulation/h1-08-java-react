e35f9509412f3a4c998e6edd71aa464777be1af8
gabyp05 <gabyapd05@gmail.com>
2024-07-26 17:04:25 -0400

    rutas doctor

---
+++ client/src/routes/DoctorRoutes.jsx
@@ -0,0 +1,12 @@
+import ProtectedRoute from "./ProtectedRoute";
+import { Dashboard } from "../pages/doctor";
+
+const DoctorRoutes = () => {
+  return (
+    <>
+      <ProtectedRoute path="/Dashboard" component={Dashboard} />
+    </>
+  );
+};
+
+export default DoctorRoutes;
